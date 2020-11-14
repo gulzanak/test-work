6f53a1e544f55865dfca48c78748b907901170fe
gulzanak <68069018+gulzanak@users.noreply.github.com>
2020-11-14 05:48:00 -0600

    Initial commit

---
+++ js/my.js
@@ -0,0 +1,12 @@
+$( document ).ready(function() {
+  
+   $( ".navbar-brand-con" ).click(function() {
+    event.preventDefault();
+    alert( "+996707411680, www.gulzana.katkeldieva@gmail.com" );
+  });
+
+});
+
+
+
+
